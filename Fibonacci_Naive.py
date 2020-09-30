085fca17fb7f58ed0c40bbaa5c29b46533f28118
Bo Cheng <bocheng1992@gmail.com>
2020-09-30 00:10:31 -0400

    Add files via upload

--- Fibonacci_Naive.py
+++ Fibonacci_Naive.py
@@ -2,6 +2,8 @@
 
 
 def Fibonacci(n):
+    # Initializing the first 2 entries
     if n <= 1:
         return n
+    # Any subsequent number is the sum of the previous two
     return Fibonacci(n-1) + Fibonacci(n-2)
